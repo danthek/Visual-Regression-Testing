df157e938518896daef65f9c740b4b554abe0fd3
danthek <abraham8507@gmail.com>
2022-08-13 14:02:49 -0500

    snapshot test: fullpage, single element, removing elements ,on emulation devices

--- tests-snapshots/__tests__/visual.test.js
+++ tests-snapshots/__tests__/visual.test.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer')
 const { toMatchImageSnapshot } = require('jest-image-snapshot')
+const { emulateDevice, emulateDesktop } = require('../../lib/helpers')
 expect.extend({ toMatchImageSnapshot }) // to use jests function with mocha's expect
 
 describe('Visual Regression Testing', () => {
@@ -8,7 +9,7 @@ describe('Visual Regression Testing', () => {
 	beforeAll(async function () {
 		// we use beforeAll, afterAll and "test""because is jest and not mocha and chai"
 		browser = await puppeteer.launch({
-			headless: false,
+			headless: true,
 			slowMo: 0,
 			devtools: false,
 		})
@@ -23,11 +24,13 @@ describe('Visual Regression Testing', () => {
 	test('Full Page Snapshot', async function () {
 		await page.goto('https://example.com/')
 		await page.waitForSelector('h1')
-		const image = await page.screenshot({ path: 'page.png', fullPage: 'true' }) // scrrenshot func is puppeteer native
+		const image = await page.screenshot({ path: 'page.png', fullPage: 'true' }) // screenshot func is puppeteer native
 		expect(image).toMatchImageSnapshot({
 			//expect + tomatch.. (jest native function)
-			failureThresholdType: 'pixel',
-			failureThreshold: 500, // if its 500 or more pixels , the test will fail
+			failureThresholdType: 'percent',
+			failureThreshold: '0.01',
+			/* failureThresholdType: 'pixel',
+			failureThreshold: 500, // if its 500 or more pixels , the test will fail */
 		})
 	})
 
@@ -37,15 +40,26 @@ describe('Visual Regression Testing', () => {
 		const image = await h1.screenshot() // we are targeting the  area of the h1 instead of the entire page.
 		expect(image).toMatchImageSnapshot({
 			failureThresholdType: 'percent',
-			failureThreshold: '0.01', // if the percentage of the pixel is more than this, it will throw an error
+			failureThreshold: '0.01',
+		})
+	})
+
+	test('Desktop Snapshot', async function () {
+		await page.goto('https://example.com/')
+		await emulateDesktop(page, 'h1', 1650, 1050)
+		const image = await page.screenshot()
+		expect(image).toMatchImageSnapshot({
+			failureThresholdType: 'percent',
+			failureThreshold: '0.01',
 		})
 	})
 
 	test('Mobile Snapshot', async function () {
-		const mobile = puppeteer.devices['iPhone X'] // remember to always save the device into a variable to use it later in the emulate function
 		await page.goto('https://example.com/')
+		/* const mobile = puppeteer.devices['iPhone X'] // remember to always save the device into a variable to use it later in the emulate function
 		await page.waitForSelector('h1') // just to validate if we are in the correct screen that we want
-		await page.emulate(mobile)
+		await page.emulate(mobile) */
+		await emulateDevice(page, 'iPhone X', 'h1')
 		const image = await page.screenshot()
 		expect(image).toMatchImageSnapshot({
 			failureThresholdType: 'percent',
@@ -53,10 +67,8 @@ describe('Visual Regression Testing', () => {
 		})
 	})
 	test('Tablet Snapshot', async function () {
-		const tablet = puppeteer.devices['iPad landscape']
 		await page.goto('https://example.com/')
-		await page.waitForSelector('h1') // just to validate if we are in the correct screen that we want
-		await page.emulate(tablet)
+		await emulateDevice(page, 'iPad landscape', 'h1')
 		const image = await page.screenshot()
 		expect(image).toMatchImageSnapshot({
 			failureThresholdType: 'percent',
@@ -64,7 +76,7 @@ describe('Visual Regression Testing', () => {
 		})
 	})
 	test('Remove Element Before Snapshot', async function () {
-		//".only"  iS A HOOK to run only a desire test inside the describe function
+		//"test.only"  iS A HOOK to run only a desire test inside the describe function
 		await page.goto('https://example.com/')
 		await page.evaluate(() => {
 			;(document.querySelectorAll('a') || []).forEach((element) =>
@@ -75,8 +87,10 @@ describe('Visual Regression Testing', () => {
 		const image = await page.screenshot({ path: 'page.png', fullPage: 'true' }) // scrrenshot func is puppeteer native
 		expect(image).toMatchImageSnapshot({
 			//expect + tomatch.. (jest native function)
-			failureThresholdType: 'pixel',
-			failureThreshold: 500, // if its 500 or more pixels , the test will fail
+			failureThresholdType: 'percent',
+			failureThreshold: '0.01',
+			/* failureThresholdType: 'pixel',
+			failureThreshold: 500, // if its 500 or more pixels , the test will fail */
 		})
 	})
 })
